eed8cee45fae55c00a940abf69776d665f3502ad
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-09-13 20:08:21 +0200

    adding moving logic, VIP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css';
-import {BasicBlock} from "./components/BasicBlock/BasicBlock.component";
+import "./App.css";
+import { BasicBlock } from "./components/BasicBlock/BasicBlock.component";
 
 function App() {
+  const move = { top: "0", left: "0" };
+  const handleKeyDown = (e, move) => {
+    e.preventDefault();
+    if (
+      e.code !== "ArrowDown" &&
+      e.code !== "ArrowLeft" &&
+      e.code !== "ArrowRight"
+    )
+      return;
+    else if (e.code === "ArrowDown") return move;
+    console.log(e.code);
+  };
   return (
-    <div className="App">
-      <BasicBlock>1</BasicBlock>
+    <div className="App" tabIndex={0} onKeyDown={handleKeyDown}>
+      <BasicBlock
+        backgroundColor={"red"}
+        top={`${move.top} vw`}
+        left={`${move.left} vw`}
+        text={12}
+        onClick={handleKeyDown}
+        onKeyDown={handleKeyDown}
+      />
     </div>
   );
 }
 
 export default App;
+
+//   make other shapes based on BasicBlock  ?? make one composite and pass it arguments instead of making one by one, that might enable random generation?
+
+//  make movement logic, if posible in same handler or better in separate?
+
+//  make colision logic
+
+//  make pieces "stick" to botom and other pieces that are already "dead"
+
+//  make row complete logic with row remove and upper rows move down
+
+//  game over logic
+
+//  score?
--- src/components/BasicBlock/BasicBlock.component.jsx
+++ src/components/BasicBlock/BasicBlock.component.jsx
@@ -1,11 +1,20 @@
 import "./BasicBlock.styles.scss";
 
-export function BasicBlock (props) {
-    const style = {
-        backgroundColor: props.backgroundColor,
-    };
+export function BasicBlock(props) {
+  const style = {
+    backgroundColor: props.backgroundColor,
+    top: props.top,
+    left: props.left,
+  };
 
-    return (
-        <div className="basicBlock">{props.text}</div>
-    );
+  return (
+    <div
+      className="basicBlock"
+      style={style}
+      onClick={props.onClick}
+      onKeyDown={props.onKeyDown}
+    >
+      {props.text}
+    </div>
+  );
 }
