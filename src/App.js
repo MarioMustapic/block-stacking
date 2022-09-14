8ea713393024b8fd3c93abff4e8619f83f0178dd
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-09-14 12:43:46 +0200

    added "gravity"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
 import { BasicBlock } from "./components/BasicBlock/BasicBlock.component";
@@ -5,6 +6,16 @@ import { BasicBlock } from "./components/BasicBlock/BasicBlock.component";
 function App() {
   const [state, setstate] = useState({ top: 0, left: 0, x: "top", y: "left" });
   console.log(state);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setstate((state) => ({
+        ...state,
+        [state.x]: state.top + 1,
+      }));
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleKeyDown = (e) => {
     e.preventDefault();
@@ -50,6 +61,7 @@ export default App;
 //   make other shapes based on BasicBlock  ?? make one composite and pass it arguments instead of making one by one, that might enable random generation?
 
 //  make movement logic, if posible in same handler or better in separate?
+//  and add block droping down ("gravity")
 
 //  make colision logic
 
