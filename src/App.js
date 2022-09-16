15e703333076686424ca7c6294e24d1ac516bc41
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-09-16 12:51:53 +0200

    Started doing colision detection

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ function App() {
     top: 0,                       //def value for starting Y-axis position(top)
     left: 0,                      //def value for starting X-axis position(centered)
     rotation: 0,                  //def value for angle of composite block
+    isInColision: false,          //def value for colision logic
     x: "top",                     //proxy for top keyword
     y: "left",                    //proxy for left keyword
-    z: "rotation",                 //proxy for rotatio keyword
+    z: "rotation",                //proxy for rotatio keyword
     text: "",                     //block text (atm no use)
-    gravityTimer: 5000,      //timer for downward movment over time  
+    gravityTimer: 5000,           //timer for downward movment over time  
     basicBlockSize: 20,           //size in pixels
     compositeBlockSize: 4,        //max height or width in number of basicBlocks
     backgroundColor: "green",     //def background color
     blockType: Math.floor(Math.random() * 7),  //block randomizer
   });
-  console.log(state);
   useEffect(() => {
     const timer = setInterval(() => {
       setstate((state) => ({
@@ -32,17 +32,17 @@ function App() {
 
   const handleKeyDown = (e) => {
     e.preventDefault();
-    if (e.code === "ArrowDown") {
+    if (e.code === "ArrowDown" && state.isInColision === false) {
       return setstate((state) => ({
         ...state,
         [state.x]: state.top + state.basicBlockSize,
       }));
-    } else if (e.code === "ArrowRight") {
+    } else if (e.code === "ArrowRight" && state.isInColision === false) {
       return setstate((state) => ({
         ...state,
         [state.y]: state.left + state.basicBlockSize,
       }));
-    } else if (e.code === "ArrowLeft") {
+    } else if (e.code === "ArrowLeft" && state.isInColision === false) {
       return setstate((state) => ({
         ...state,
         [state.y]: state.left - state.basicBlockSize,
@@ -80,6 +80,7 @@ export default App;
 //   make other shapes based on BasicBlock  ?? make one composite and pass it arguments instead of making one by one, that might enable random generation?
 
 //  make movement logic, if posible in same handler or better in separate?
+//  add rotation
 //  and add block droping down ("gravity")
 
 //  make colision logic
--- src/components/BasicBlock/BasicBlock.component.jsx
+++ src/components/BasicBlock/BasicBlock.component.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import "./BasicBlock.styles.scss";
 
 export function BasicBlock(props) {
+  const colisionDetection = () => {
+    let compositeBlockColision1 = document.querySelector(".basicBlock");
+    console.log(compositeBlockColision1);
+    let rect = compositeBlockColision1.getBoundingClientRect();
+    console.log(rect);
+  };
   const style = {
     backgroundColor: props.blockState.backgroundColor,
     height: props.blockState.basicBlockSize,
@@ -8,6 +15,9 @@ export function BasicBlock(props) {
     top: props.compositeBlock.top,
     left: props.compositeBlock.left,
   };
+  useEffect(() => {
+    colisionDetection();
+  }, []);
   return (
     <div
       className="basicBlock"
