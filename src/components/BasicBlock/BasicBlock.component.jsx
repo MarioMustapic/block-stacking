c9bd867d2821cd1eec78da259ee33cde8a60401e
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-10-06 10:21:35 +0200

    Rework of rotation

--- src/components/BasicBlock/BasicBlock.component.jsx
+++ src/components/BasicBlock/BasicBlock.component.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-import { useCallback } from "react";
 import { useState } from "react";
 import "./BasicBlock.styles.scss";
 
 export function BasicBlock(props) {
-  const [basicBlockState] = useState(props.compositeBlockState);
+  const [basicBlockState, setBasicBlockState] = useState(
+    props.compositeBlockState
+  );
   // const [rotationOffset, setCompositeBlockOffset] = useState({ x: 0, y: 0 });
   const [compositeBlockOffset, setCompositeBlockOffset] = useState({
     x: props.compositeBlock.x,
@@ -17,118 +18,116 @@ export function BasicBlock(props) {
     compositeBlockOffset.x +
     Math.floor(basicBlockState.playingFieldWidth / 2);
 
-  const rotation = useCallback(() => {
-    let x = compositeBlockOffset.x;
-    let y = compositeBlockOffset.y;
-    if (props.compositeBlock.name === "O-block") return;
-    // if (props.compositeBlock.length === "evenLength") return;
-    switch (x) {
-      case -1:
-        x = 1;
-        y = 3; //I-block band-aid for now
-        break;
-      case 0:
-        switch (y) {
-          case 0:
-            x = 2;
-            y = 0;
-            break;
-          case 1:
-            x = 1;
-            y = 0;
-            break;
-          case 2:
-            x = 0;
-            y = 0;
-            break;
-          default:
-            console.log("something broke", y);
-        }
-        break;
-      case 1:
-        switch (y) {
-          case 0:
-            x = 2;
-            y = 1;
-            break;
-          case 1:
-            x = 1;
-            y = 1;
-            break;
-          case 2:
-            x = 0;
-            y = 1;
-            break;
-          case 3:
-            x = -1;
-            y = 1; //I-block band-aid for now
-            break;
-          default:
-            console.log("something broke", y);
-        }
-        break;
-      case 2:
-        switch (y) {
-          case 0:
-            x = 2;
-            y = 2;
-            break;
-          case 1:
-            x = 1;
-            y = 2;
-            break;
-          case 2:
-            x = 0;
-            y = 2;
-            break;
-          default:
-            console.log("something broke", y);
-        }
-        break;
-      default:
-        console.log("something broke", x);
+  let x = compositeBlockOffset.x;
+  let y = compositeBlockOffset.y;
+  let blockCollisionRotate = false;
+
+  switch (x) {
+    case -1:
+      x = 1;
+      y = 3; //I-block band-aid for now
+      break;
+    case 0:
+      switch (y) {
+        case 0:
+          x = 2;
+          y = 0;
+          break;
+        case 1:
+          x = 1;
+          y = 0;
+          break;
+        case 2:
+          x = 0;
+          y = 0;
+          break;
+        default:
+          console.log("something broke", y);
+      }
+      break;
+    case 1:
+      switch (y) {
+        case 0:
+          x = 2;
+          y = 1;
+          break;
+        case 1:
+          x = 1;
+          y = 1;
+          break;
+        case 2:
+          x = 0;
+          y = 1;
+          break;
+        case 3:
+          x = -1;
+          y = 1; //I-block band-aid for now
+          break;
+        default:
+          console.log("something broke", y);
+      }
+      break;
+    case 2:
+      switch (y) {
+        case 0:
+          x = 2;
+          y = 2;
+          break;
+        case 1:
+          x = 1;
+          y = 2;
+          break;
+        case 2:
+          x = 0;
+          y = 2;
+          break;
+        default:
+          console.log("something broke", y);
+      }
+      break;
+    default:
+      console.log("something broke", x);
+  }
+
+  useEffect(() => {
+    if (props.compositeBlockState.rotation === 0) return;
+    if (props.compositeBlockState.rotation === basicBlockState.rotation) return;
+    if (blockCollisionRotate === false) {
+      setCompositeBlockOffset({ x, y });
+      setBasicBlockState((state) => ({
+        ...state,
+        rotation: basicBlockState.rotation + 1,
+      }));
+      return;
+    } else if (blockCollisionRotate === true) {
+      props.setCompositeBlockState((state) => ({
+        ...state,
+        rotation: props.compositeBlockState.rotation - 1,
+      }));
     }
-    let blockCollisionRotate = false;
-    let isInColisionRotate = props.playingFieldBlocksCords.filter(
-      //filters blocks with same cordinates if we move right one block
-      (e) => e.left === calculatedX && e.top === calculatedY
-    );
-    if (isInColisionRotate.length > 0) blockCollisionRotate = true;
-    // console.log(!(calculatedY < props.defBlockState.playingFieldHeight));
-    console.log(!(calculatedX > 0));
-    // console.log(!(calculatedX < props.defBlockState.playingFieldWidth));
-    // props.compositeBlockState.isInColision.rotation[props.indexkey] =
-    //   !(calculatedY < props.defBlockState.playingFieldHeight - 1) ||
-    //   !(calculatedX < props.defBlockState.playingFieldWidth - 1) ||
-    //   !(calculatedX > 0) ||
-    //   blockCollisionRotate;
-    if (blockCollisionRotate === false) setCompositeBlockOffset({ x, y });
   }, [
-    // props.compositeBlockState.isInColision.rotation,
-    // props.defBlockState.playingFieldWidth,
-    // props.defBlockState.playingFieldHeight,
-    props.compositeBlock.name,
-    props.playingFieldBlocksCords,
-    // props.indexkey,
-    compositeBlockOffset.x,
-    compositeBlockOffset.y,
-    calculatedX,
-    calculatedY,
+    props.compositeBlockState.rotation,
+    basicBlockState.rotation,
+    blockCollisionRotate,
+    props,
+    x,
+    y,
   ]);
-  useEffect(() => {
-    if (props.compositeBlockState.rotation !== 0) {
-      let abc = document.querySelector(`.basicBlock__${props.indexkey}`);
-      abc.dispatchEvent(new Event("rotate"));
-    }
-  }, [props.compositeBlockState.rotation, props.indexkey]);
-  useEffect(() => {
-    const compositeBlock = document.querySelector(
-      `.basicBlock__${props.indexkey}`
-    );
-    compositeBlock.addEventListener("rotate", () => {
-      rotation();
-    });
-  });
+
+  // useEffect(() => {
+  //   if (props.compositeBlockState.rotation !== 0) {
+  //     let abc = document.querySelector(`.basicBlock__${props.indexkey}`);
+  //     abc.dispatchEvent(new Event("rotate"));
+  //   }
+  // }, [props.compositeBlockState.rotation, props.indexkey]);
+  // useEffect(() => {
+  //   const compositeBlock = document.querySelector(
+  //     `.basicBlock__${props.indexkey}`
+  //   );
+  //   compositeBlock.addEventListener("rotate", () => {
+  //     rotation();
+  //   });
+  // });
 
   useEffect(() => {
     let sendCord = true;
