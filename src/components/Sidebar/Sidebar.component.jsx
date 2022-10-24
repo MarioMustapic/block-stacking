dc3157d8c7f0b2d7edfa2fa478b3712ddc25e30c
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-10-24 13:30:45 +0200

    Sidebar styles

--- src/components/Sidebar/Sidebar.component.jsx
+++ src/components/Sidebar/Sidebar.component.jsx
@@ -2,15 +2,19 @@ import { DefState } from "../../models/DefState";
 import "./Sidebar.styles.scss";
 
 export function Sidebar(props) {
-  console.log(DefState);
+  let scaleCoef = 21; //scaling factor for basicBlockSize
+  if ("ontouchstart" in document.documentElement === true) scaleCoef = 28;
+  let newBasicBlockSize = Math.floor(window.innerHeight / scaleCoef);
 
+  const defState = new DefState({ basicBlockSize: newBasicBlockSize });
+  console.log(defState);
   return (
     <div
       className="sidebar"
-      //   style={{
-      //     width: `${state.basicBlockSize * state.playingFieldWidth}px`,
-      //     height: `${state.basicBlockSize * state.playingFieldHeight}px`,
-      //   }}
+      style={{
+        width: `${defState.basicBlockSize * 6}px`,
+        height: `${defState.basicBlockSize * defState.playingFieldHeight}px`,
+      }}
     ></div>
   );
 }
--- src/models/DefState.js
+++ src/models/DefState.js
@@ -18,50 +18,55 @@ export class DefState {
     gameOver,
     mode,
   }) {
-    this.top = top;
-    this.left = left;
-    this.rotation = rotation;
-    this.isInColision = isInColision;
-    this.toAppend = toAppend;
-    this.toRenderCompositeBlock = toRenderCompositeBlock;
-    this.toRotate = toRotate;
-    this.playingFieldWidth = playingFieldWidth;
-    this.playingFieldHeight = playingFieldHeight;
-    this.text = text;
-    this.gravityTimer = gravityTimer;
-    this.basicBlockSize = basicBlockSize;
-    this.compositeBlockSize = compositeBlockSize;
-    this.backgroundColor = backgroundColor;
-    this.blockType = blockType;
-    this.gameOver = gameOver;
-    this.mode = mode;
+    this.top = top || 0;
+    this.left = left || 0;
+    this.rotation = rotation || 0;
+    this.isInColision = isInColision || {
+      down: [false, false, false, false],
+      right: [false, false, false, false],
+      left: [false, false, false, false],
+      rotation: [false, false, false, false],
+    };
+    this.toAppend = toAppend || false;
+    this.toRenderCompositeBlock = toRenderCompositeBlock || true;
+    this.toRotate = toRotate || true;
+    this.playingFieldWidth = playingFieldWidth || 10;
+    this.playingFieldHeight = playingFieldHeight || 20;
+    this.text = text || "";
+    this.gravityTimer = gravityTimer || 700;
+    this.basicBlockSize = basicBlockSize || 25;
+    this.compositeBlockSize = compositeBlockSize || 5;
+    this.backgroundColor = backgroundColor || "";
+    this.blockType = blockType || Math.floor(Math.random() * 7);
+    this.gameOver = gameOver || false;
+    this.mode = mode || "desktop";
   }
-  static fromObject(dataObject) {
+  static fromObject() {
     const defState = new DefState({
-        top: 0,                       //def value for starting Y-axis position(top)
-        left: 0,                      //def value for starting X-axis position(centered)
-        rotation: 0,                  //def value for angle of composite block
-        isInColision:                 //def value for colision logic
-          {
-            down:[false,false,false,false,],
-            right:[false,false,false,false,],
-            left:[false,false,false,false,],
-            rotation:[false,false,false,false,],
-          },
-        toAppend: false,
-        toRenderCompositeBlock: true,
-        toRotate:true,
-        playingFieldWidth: 10,
-        playingFieldHeight: 20,
-        text: "",                     //block text (atm no use)
-        gravityTimer: 700,         //timer for downward movment over time
-        basicBlockSize: 25,           //size in pixels
-        compositeBlockSize: 5,        //max height or width in number of basicBlocks
-        backgroundColor: "",          //def background color
-        blockType: Math.floor(Math.random() * 7),  //block randomizer
-        gameOver: false,              //if true, start over
-        mode: "desktop",              //def display mode    });
-        });
+      top: 0, //def value for starting Y-axis position(top)
+      left: 0, //def value for starting X-axis position(centered)
+      rotation: 0, //def value for angle of composite block
+      //def value for colision logic
+      isInColision: {
+        down: [false, false, false, false],
+        right: [false, false, false, false],
+        left: [false, false, false, false],
+        rotation: [false, false, false, false],
+      },
+      toAppend: false,
+      toRenderCompositeBlock: true,
+      toRotate: true,
+      playingFieldWidth: 10,
+      playingFieldHeight: 20,
+      text: "", //block text (atm no use)
+      gravityTimer: 700, //timer for downward movment over time
+      basicBlockSize: 25, //size in pixels
+      compositeBlockSize: 5, //max height or width in number of basicBlocks
+      backgroundColor: "", //def background color
+      blockType: Math.floor(Math.random() * 7), //block randomizer
+      gameOver: false, //if true, start over
+      mode: "desktop", //def display mode    });
+    });
 
     return defState;
   }
